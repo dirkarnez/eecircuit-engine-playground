a50eb36c107b346669b4800694e2ebe2cffd9141
dirkarnez <smalldirkalex@gmail.com>
2025-12-24 16:50:51 +0800

    - wip

---
+++ global.d.ts
@@ -0,0 +1,14 @@
+// Not working, need to hack https://github.com/microsoft/vscode/tree/main/extensions/html-language-features
+// import type { Simulation } from "./main.d.ts";
+
+// declare global {
+//     // Note the capital "W"
+//     interface Window { 
+//         "EEcircuit-engine": { 
+//             Simulation: Simulation
+//         }
+//     }
+// }
+// declare interface Window {
+//   opts: Object
+// }
